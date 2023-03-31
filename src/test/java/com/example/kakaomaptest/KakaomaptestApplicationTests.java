5ffaacae94199633d4480d0668a46bd1f109be45
jueun0725 <kimjueun010@naver.com>
2023-03-31 15:06:08 +0900

    first commit

---
+++ src/test/java/com/example/kakaomaptest/KakaomaptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaomaptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaomaptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
