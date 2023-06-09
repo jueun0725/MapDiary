a2dfb92d227331ef84210aaa67f9e882d478491e
장정윤 <jeinie9179@naver.com>
2023-06-09 18:34:33 +0900

    지도 뷰

--- src/main/java/com/example/kakaomaptest/controller/LocationController.java
+++ src/main/java/com/example/kakaomaptest/controller/LocationController.java
@@ -1,13 +1,24 @@
 package com.example.kakaomaptest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LocationController {
 
-    @GetMapping("/location")
+    @GetMapping("/location/{placeName}")
     public String showLocation() {
+        return "location";
+    }
+
+    @PostMapping("/location/{placeName}")
+    public String mapLocation(@PathVariable String placeName, Model model) {
+        model.addAttribute("placeName", placeName);
+        System.out.println("model: " + model);
         return "location";
     }
 }
